34d20a3416b7dc4e7322bc1114c4cfeee980cd9d
Jennifer Massi <jennifer.massi@mscoding.org>
2022-12-29 12:08:17 -0800

    Apples

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Apples
+{
+    internal class Program
+    {
+        static void Main(string[] args) { 
+
+            string apple1 = "gala";
+            string apple2 = " pink lady";
+            string apple3 = " golden delicious";
+            string apple4 = " fuji";
+            string apple5 = " granny smith";
+            string apple6 = " honey crisp";
+
+            Console.WriteLine(apple1 + apple2 + apple3 + apple4 + apple5 + apple6);
+
+            apple5 = " extinct";
+          
+            Console.WriteLine(apple1 + apple2 + apple2 + apple3 + apple4 + apple5 + apple6);
+           
+        }//main
+    }//class
+}//namespace
